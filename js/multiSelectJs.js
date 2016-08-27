4308948b3dd74a98cfb565b6f4bf1545ca875ff6
cactuscloud <admin@cactus.cloud>
2016-08-27 16:45:18 +1000

    Initialization continued

--- js/multiSelectJs.js
+++ js/multiSelectJs.js
@@ -13,6 +13,9 @@
  *						[{n: "label", v: "value"}, ...]
  *				
  *	Initialization:	This can be initialized from multiple elements and THIS IS NOT DONE YET!
+ *
+ *	Polyfills:		The following polyfills are included with this file:
+ *						- Array.isArray(x) : Boolean
  */
 
 
@@ -20,6 +23,7 @@ function multiSelectJs(el, options) {
 	//Initialization variables
 	this.initialized = false;
 	this.initializing = false;
+	this.hasRequest = false;//Is there a current server request being fired
 	
 	//Behavioral settings
 	this.maxSelections = 5;//Number of items the user can select
@@ -31,13 +35,26 @@ function multiSelectJs(el, options) {
 	//Stored and selected data
 	this.dataSet = [];//The stored values from which the user can search
 	this.selections = [];//The current selections
+	this.results = [];
 	
-	//The search method.  This specifies where the multiSelect values come from when the user
+	//The search funciton.  This specifies where the multiSelect values come from when the user
 	//performs a search.  If left unset, the values will be sourced from the dataSet variable.
+	//This function should accept one search string and optionally an instance of "this".  This 
+	//function should call multiSelectJs.searchCallback(Array data) upon completion.
 	this.searchMethod = this.localSearch;
-	
+	this.searchTerms = null;//The current search terms
+	this.lastSearch = null;//The previous search terms
 	
 	this.init(el);
+}
+
+/**
+*	Array.isArray polyfill (mozilla)
+*/
+if (!Array.isArray) {
+	Array.isArray = function(arg) {
+		return Object.prototype.toString.call(arg) === '[object Array]';
+	};
 }
 
 multiSelectJs.prototype.init = function(el, options) {
@@ -58,7 +75,6 @@ multiSelectJs.prototype.init = function(el, options) {
 	this.parseOptions(options);
 	
 	
-	
 	this.initialized = true;
 	this.initializing = false;
 }
@@ -68,7 +84,9 @@ multiSelectJs.prototype.parseOptions = function(options) {
 	
 }
 
-
+/**
+ *	The multiSelectJs destructor
+ */
 multiSelectJs.prototype.destroy = function() {
 	try {
 		this.template.multiSelectJs = null;
@@ -76,17 +94,110 @@ multiSelectJs.prototype.destroy = function() {
 	} catch(Exception e) {}
 	try {
 		this.dataSet = null;
+		this.selections = null;
+		this.results = null;
 		this.searchMethod = null;
 	} catch(Exception e) {}
 	this.initialized = false;
 	this.initializing = false;
 }
 
-multiSelectJs.prototype.localSearch = function() {
-	
-}
-	
-multiSelectJs.prototype.searchCallback = function(data) {
-	
+
+
+
+/**
+ *	Initiates a search for the user entered string.  This function ensures that only one search request
+ *	is fired at any one time and that search requests only proceed when search terms exist, and have
+ *	changed since the last search.
+ *
+ *	This function will fire the search function stored in multiSelectJs.searchMethod(String terms).  This
+ *	function will either be the default search function (multiSelectJs.localSearch(String terms)) or
+ *	a custom, user specified search function.
+ *
+ *	The function defined in this.searchMethod should then fire multiSelectJs.searchCallback(Array data)
+ *	upon completion.
+ */
+multiSelectJs.prototype.performSearch = function() {
+	//Ensure only one request fires at a time and that the search terms exist and have changed
+	if(this.hasRequest === true || this.searchTerms == null || this.searchTerms == this.lastSearch) return;
+	this.hasRequest = true;
+	try {
+		this.lastSearch = this.searchTerms;
+		this.searchMethod(this.searchTerms, this);
+	} catch(Exception e) {
+		this.hasRequest = false;
+	}
 }
 
+/**
+ *	The default search method.  This finds the closest matching values that exist within the this.dataSet
+ *	variable and passes them to this.searchCallback.
+ */
+multiSelectJs.prototype.localSearch = function(terms) {
+	//Do search here
+	
+	
+	this.searchCallback(null);
+}
+	
+/**
+ *	The callback function that should be fired after a user has performed a search.  This method will
+ *	trigger another search if another search has been queued.  This method will also validate the
+ *	supplied data and trigger the appropriate GUI events as required.
+ */
+multiSelectJs.prototype.searchCallback = function(data) {
+	var hadRequest = this.hasRequest;
+	
+	//Do call back action here
+	if(data == null) {
+		//There has been an error
+		/* HANDLE THIS */
+		
+	} else if(typeof data == "string") {
+		//There has been an error and an error message has been supplied
+		this.hasRequest = false;
+		throw new Error("There has been an error fetching multiSelectJs data: " + data);
+	} else if(!Array.isArray(data)) {
+		//An unknown error has occurred
+		console.log("Invalid data supplied:");
+		console.log(data);
+		this.hasRequest = false;
+		throw new TypeError("multiSelectJs has received an invalid data type in its search callback function");
+	} else {
+		//Data looking okay, so far...
+		this.results = null;
+		this.results = multiSelectJs.parseData(data, this.maxSelections);
+		
+		/* NOW DO THE GUI */
+	}
+	
+	this.hasRequest = false;
+	if(hadRequest) this.performSearch();
+}
+
+
+
+/*
+ *	Static functions
+ */
+ 
+/**
+ *	"Static" function that checks and limits the size of an array of data values for a multiSelectJs
+ *	instance.
+ */
+multiSelectJs.parseData = function(data, limit) {
+	if(!Array.isArray(data)) throw new TypeError("multiSelectJs has received an invalid data type in its search callback function");
+	var x, out = [];
+	var i = 0, j = data.length;
+	if(typeof limit == "number" && limit < j) j = limit;
+	for(; i < j; i++) {
+		x = data[i];
+		if(!typeof x.v == "string" || !typeof x.n == "string") {
+			console.log("Invalid data supplied:");
+			console.log(x);
+			throw new TypeError("multiSelectJs has received invalid result data at index " + i);
+		}
+		out.push(x);
+	}
+	return out;	
+}
